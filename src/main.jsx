b78fbdfb6ab975496948567532cfc99892ef4062
Rumanito12 <ady_9697@yahoo.com>
2024-04-10 10:39:45 +0200

    PATCH: se modifica el archivo app para poder desplegar el proyecto.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import store from "./App/store.js";
+import store from "./app/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import persistStore from "redux-persist/es/persistStore";
+import persistStore from "redux-persist/es/persistStore.js";
 import {Provider} from "react-redux"
 import "./main.scss"
 
