a0529d4d0a4a4714b7d867514accf5003ef76157
Rumanito12 <ady_9697@yahoo.com>
2024-04-13 16:17:16 +0200

    ADD: se añaden mas opciones al menu una vez este logueado.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -20,38 +20,50 @@ export const Home = () => {
     <Container className="d-flex home-design">
       <Col>
         <Row>
-          <Col className="text-center" xs={12} md={12}>
+          <Col className={token?("text-center master-2-design"):("text-center master-design")} xs={12} md={12}>
             <h1>Master Mind</h1>
           </Col>
         </Row>
         {token ? (
           <>
-            <h1>Hola Sr.{user.name}</h1>
-            <Row>
-              <Col xs={2} md={4}></Col>
-              <Col
-                onClick={()=>LogOut()}
-                className="menu-option"
-                xs={8}
-                md={4}
-              >
+            <h4 className="text-center">Hola Sr.{user.name}</h4>
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/mastermind" className="menu-option" xs={8} md={4}>
+                <h2>Jugar</h2>
+              </Col>
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/profile_user" className="menu-option" xs={8} md={4}>
+                <h2>Perfil de Usuario</h2>
+              </Col>
+            </Row>
+            {user.role === "admin" && (
+              <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/profile_admin_users" className="menu-option" xs={8} md={4}>
+                <h2>Perfil de Usuario</h2>
+              </Col>
+            </Row>
+            )}
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/" className="menu-option" xs={8} md={4}>
+                <h2>Como Jugar</h2>
+              </Col>
+            </Row>
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/" onClick={()=>LogOut()} className="menu-option" xs={8} md={4}>
                 <h2>Log Out</h2>
               </Col>
             </Row>
           </>
         ) : (
           <>
-            <Row>
-              <Col xs={2} md={4}></Col>
-              <Col as={Link} to="/login_user" className="menu-option" xs={8} md={4}
-              >
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/login_user" className="menu-option" xs={9} md={4}>
                 <h2>Iniciar Sesion</h2>
               </Col>
             </Row>
-            <Row>
-              <Col xs={2} md={4}></Col>
-              <Col as={Link} to="/register_user" className="menu-option" xs={8} md={4}
-              >
+            <Row className="d-flex justify-content-center">
+              <Col as={Link} to="/register_user" className="menu-option" xs={9} md={4}>
                 <h2>Registrate</h2>
               </Col>
             </Row>
