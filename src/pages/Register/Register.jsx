37ba98cf469a5384d225be04a6637f344ab54a55
Rumanito12 <ady_9697@yahoo.com>
2024-04-13 14:27:32 +0200

    ADD: se añade vista loading para registro de usuario.

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -10,6 +10,7 @@ import { login, register } from "../../service/apiCalls";
 import { userDate, userLogin } from "../userSlice";
 import { ToastContainer, toast } from "react-toastify";
 import { validate } from "../../service/useFul";
+import loading_gif from "../../img/loading.gif";
 import "react-toastify/dist/ReactToastify.css";
 import "./Register.scss";
 
@@ -29,8 +30,16 @@ export const Register = () => {
     emailError: "",
     nicknameError: "",
     passwordError: "",
+    otherError: "",
   });
   const token = useSelector(userDate).credentials;
+  const [loading, setLoading] = useState(false);
+  const handleOtherError = (errorMessage) => {
+    setErrorData((prevState) => ({
+      ...prevState,
+      otherError: errorMessage,
+    }));
+  };
 
   const tokenExist = (tokenEx) => {
     if (tokenEx) {
@@ -49,8 +58,8 @@ export const Register = () => {
     }));
   };
 
-   //chequeo de errores para los inputs
-   const checkError = (e) => {
+  //chequeo de errores para los inputs
+  const checkError = (e) => {
     let error = "";
     error = validate(e.target.name, e.target.value);
     setErrorData((prevState) => ({
@@ -60,7 +69,9 @@ export const Register = () => {
   };
 
   useEffect(() => {
-    const lastError = Object.values(errorData).reverse().find(error => error !== "");
+    const lastError = Object.values(errorData)
+      .reverse()
+      .find((error) => error !== "");
     if (lastError) {
       toast.error(lastError, {
         position: "top-right",
@@ -74,170 +85,215 @@ export const Register = () => {
       });
     }
   }, [errorData]);
-  
 
   const registerHand = (data) => {
     data.email = data.email.toLowerCase();
-    register(data)
-      .then(() => {
-        const dataToLogin = {
-          email: data.email,
-          password: data.password,
-        };
-        login(dataToLogin)
-          .then((res) => {
-            dispatch(userLogin({ credentials: res.token, user: res.data }));
+
+    if (
+      data.name !== "" &&
+      data.last_name !== "" &&
+      data.email !== "" &&
+      data.nickname !== "" &&
+      data.password !== ""
+    ) {
+      if (
+        errorData.nameError === "" &&
+        errorData.last_nameError === "" &&
+        errorData.emailError === "" &&
+        errorData.nicknameError === "" &&
+        errorData.passwordError === ""
+      ) {
+        setLoading(true);
+        register(data)
+          .then(() => {
+            const dataToLogin = {
+              email: data.email,
+              password: data.password,
+            };
+            login(dataToLogin)
+              .then((res) => {
+                dispatch(userLogin({ credentials: res.token, user: res.data }));
+                setLoading(false);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           })
           .catch((err) => {
-            console.log(err);
+            setOtherError(err);
           });
-      })
-      .catch((err) => {
-        setOtherError(err);
-      });
+      }
+      {
+        handleOtherError("¡Hay ERRORES pendientes!");
+      }
+    } else {
+      handleOtherError("¡Campos Vacios!");
+    }
   };
 
   return (
     <Container className="register-design">
-      <Row className="d-flex justify-content-center">
-        <Col xs={12} md={8}>
-          <Row className="file-input-design">
-            <Col xs={12} md={4}>
-              <label htmlFor="name">Nombre : </label>
-            </Col>
+      {loading ? (
+        <Row className="d-flex justify-content-center loading-design">
+          <Col xs={12} md={8}>
+            <img src={loading_gif} alt="loading gif" />
+          </Col>
+        </Row>
+      ) : (
+        <>
+          <Row className="d-flex justify-content-center">
             <Col xs={12} md={8}>
-              <Custom_Input
-                name={"name"}
-                placeholder={"Nombre"}
-                type={"text"}
-                handler={inputHandler}
-                handlerError={checkError}
-                custom={errorData.nameError?("input-error"):("")}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={8}>
-          <Row className="file-input-design">
-            <Col xs={12} md={4}>
-              <label htmlFor="last_name">Apellidos : </label>
-            </Col>
-            <Col xs={12} md={8}>
-              <Custom_Input
-                name={"last_name"}
-                placeholder={"Apellidos"}
-                type={"text"}
-                handler={inputHandler}
-                handlerError={checkError}
-                custom={errorData.last_nameError?("input-error"):("")}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={8}>
-          <Row className="file-input-design">
-            <Col xs={12} md={4}>
-              <label htmlFor="email">Email : </label>
-            </Col>
-            <Col xs={12} md={8}>
-              <Custom_Input
-                name={"email"}
-                placeholder={"Correo Electronico"}
-                type={"Email"}
-                handler={inputHandler}
-                handlerError={checkError}
-                custom={errorData.emailError?("input-error"):("")}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={8}>
-          <Row className="file-input-design">
-            <Col xs={12} md={4}>
-              <label htmlFor="nickname">Nombre de Usuario : </label>
-            </Col>
-            <Col xs={12} md={8}>
-              <Custom_Input
-                name={"nickname"}
-                placeholder={"Nombre de Usuario"}
-                type={"text"}
-                handler={inputHandler}
-                handlerError={checkError}
-                custom={errorData.nicknameError?("input-error"):("")}
-              />
-            </Col>
-          </Row>
-        </Col>
-        <Col xs={12} md={8}>
-          <Row className="file-input-design">
-            <Col xs={12} md={4}>
-              <label htmlFor="password">Contraseña : </label>
-            </Col>
-            <Col xs={12} md={8}>
-              <Custom_Input
-                name={"password"}
-                placeholder={"Contraseña"}
-                type={"password"}
-                handler={inputHandler}
-                handlerError={checkError}
-                custom={errorData.passwordError?("input-error"):("")}
-              />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center row-buttons-register">  
-        <Col xs={12} md={12}>
-          <Row className="d-flex justify-content-center buttons-design">
-            <Col xs={9} md={5}>
-              <Row className="d-flex justify-content-center">
-                <Col xs={6} md={5} className="d-flex justify-content-center">
-                  <Row>
-                    <Col as={Link} to="/" className="custom_button">
-                      Menu
-                    </Col>
-                  </Row>
+              <Row className="file-input-design">
+                <Col xs={12} md={4}>
+                  <label htmlFor="name">Nombre : </label>
                 </Col>
-                <Col xs={6} md={5} className="d-flex justify-content-center">
-                  <Custom_Button
-                    name={"Registrate"}
-                    clase={"custom_button"}
-                    clickHandler={registerHand}
-                    data={registerData}
+                <Col xs={12} md={8}>
+                  <Custom_Input
+                    name={"name"}
+                    placeholder={"Nombre"}
+                    type={"text"}
+                    handler={inputHandler}
+                    handlerError={checkError}
+                    custom={errorData.nameError ? "input-error" : ""}
                   />
                 </Col>
               </Row>
             </Col>
+            <Col xs={12} md={8}>
+              <Row className="file-input-design">
+                <Col xs={12} md={4}>
+                  <label htmlFor="last_name">Apellidos : </label>
+                </Col>
+                <Col xs={12} md={8}>
+                  <Custom_Input
+                    name={"last_name"}
+                    placeholder={"Apellidos"}
+                    type={"text"}
+                    handler={inputHandler}
+                    handlerError={checkError}
+                    custom={errorData.last_nameError ? "input-error" : ""}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={8}>
+              <Row className="file-input-design">
+                <Col xs={12} md={4}>
+                  <label htmlFor="email">Email : </label>
+                </Col>
+                <Col xs={12} md={8}>
+                  <Custom_Input
+                    name={"email"}
+                    placeholder={"Correo Electronico"}
+                    type={"Email"}
+                    handler={inputHandler}
+                    handlerError={checkError}
+                    custom={errorData.emailError ? "input-error" : ""}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={8}>
+              <Row className="file-input-design">
+                <Col xs={12} md={4}>
+                  <label htmlFor="nickname">Nombre de Usuario : </label>
+                </Col>
+                <Col xs={12} md={8}>
+                  <Custom_Input
+                    name={"nickname"}
+                    placeholder={"Nombre de Usuario"}
+                    type={"text"}
+                    handler={inputHandler}
+                    handlerError={checkError}
+                    custom={errorData.nicknameError ? "input-error" : ""}
+                  />
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12} md={8}>
+              <Row className="file-input-design">
+                <Col xs={12} md={4}>
+                  <label htmlFor="password">Contraseña : </label>
+                </Col>
+                <Col xs={12} md={8}>
+                  <Custom_Input
+                    name={"password"}
+                    placeholder={"Contraseña"}
+                    type={"password"}
+                    handler={inputHandler}
+                    handlerError={checkError}
+                    custom={errorData.passwordError ? "input-error" : ""}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center row-buttons-register">
             <Col xs={12} md={12}>
-              <Row className="d-flex justify-content-center">
-                <Col xs={6} md={5} className="d-flex justify-content-center">
-                  <Row>
+              <Row className="d-flex justify-content-center buttons-design">
+                <Col xs={9} md={5}>
+                  <Row className="d-flex justify-content-center">
                     <Col
-                      as={Link}
-                      to="/login_user"
-                      className="custom_button button-register"
+                      xs={6}
+                      md={5}
+                      className="d-flex justify-content-center"
                     >
-                      Iniciar Session
+                      <Row>
+                        <Col as={Link} to="/" className="custom_button">
+                          Menu
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col
+                      xs={6}
+                      md={5}
+                      className="d-flex justify-content-center"
+                    >
+                      <Custom_Button
+                        name={"Registrate"}
+                        clase={"custom_button"}
+                        clickHandler={registerHand}
+                        data={registerData}
+                      />
+                    </Col>
+                  </Row>
+                </Col>
+                <Col xs={12} md={12}>
+                  <Row className="d-flex justify-content-center">
+                    <Col
+                      xs={6}
+                      md={5}
+                      className="d-flex justify-content-center"
+                    >
+                      <Row>
+                        <Col
+                          as={Link}
+                          to="/login_user"
+                          className="custom_button button-register"
+                        >
+                          Iniciar Session
+                        </Col>
+                      </Row>
                     </Col>
                   </Row>
                 </Col>
               </Row>
             </Col>
           </Row>
-        </Col>
-      </Row>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </>
+      )}
     </Container>
   );
 };
