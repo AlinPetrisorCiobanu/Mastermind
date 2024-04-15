2a14b31786e4d2e74dc34cc91cbdf02cbe5a334e
Rumanito12 <ady_9697@yahoo.com>
2024-04-15 17:02:03 +0200

    UPDATE: se mejora el diseño de la vista profile.

--- src/pages/Profiles/Profile.jsx
+++ src/pages/Profiles/Profile.jsx
@@ -1,16 +1,76 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { userDate } from "../userSlice";
+import { useSelector } from "react-redux";
 import "./Profile.scss";
+import { Custom_Button } from "../../common/Button/Buttons";
 
 export const Profile = () => {
+    const user = useSelector(userDate).user;
+
   return (
-    <Container>
-      <Row>
-        <Col xs={12} md={12}>
-          <Row className="d-flex justify-content-center">
-            <Col xs={12} md={6} className="card">
-              <h1>Profile</h1>
+    <Container className="profile-design">
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} md={8} className="card-design">
+          <Row className="d-flex justify-content-center row-card">
+            <Col xs={12} md={12} className="">
+              <Row>
+                <Col xs={12} md={6}>
+                  <h5>Nombre : </h5>
+                </Col>
+                <Col xs={12} md={6}>
+                  <h3>{user.name}</h3>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center row-card">
+            <Col xs={12} md={12} className="">
+              <Row>
+                <Col xs={12} md={6}>
+                  <h5>Apellidos : </h5>
+                </Col>
+                <Col xs={12} md={6}>
+                  <h3>{user.last_name}</h3>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center row-card">
+            <Col xs={12} md={12} className="">
+              <Row>
+                <Col xs={12} md={6}>
+                  <h5>Email : </h5>
+                </Col>
+                <Col xs={12} md={6}>
+                  <h3>{user.email}</h3>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center row-card">
+            <Col xs={12} md={12} className="">
+              <Row>
+                <Col xs={12} md={6}>
+                  <h5>Nombre de Usuario : </h5>
+                </Col>
+                <Col xs={12} md={6}>
+                  <h3>{user.nickname}</h3>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center row-card">
+            <Col xs={12} md={12} className="">
+              <Row>
+                <Col xs={12} md={6} className="d-flex justify-content-center button-design">
+                  <Custom_Button name={"Modificar"} />
+                </Col>
+                <Col xs={12} md={6} className="d-flex justify-content-center button-design">
+                <Custom_Button name={"Eliminar"} />
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Col>
